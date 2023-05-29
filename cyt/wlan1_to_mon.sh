285dea4d65ab4a429749c713dabed87c630ab208
H179922 <smoof017@yahoo.com>
2023-05-28 22:39:05 -0400

    Update wlan1_to_mon.sh
    modified to use ip command suite

--- cyt/wlan1_to_mon.sh
+++ cyt/wlan1_to_mon.sh
@@ -1,6 +1,6 @@
-sudo ifconfig wlan0 down
-sudo iwconfig wlan0 mode monitor
-sudo ifconfig wlan0 up
-sudo ifconfig wlan1 down
-sudo iwconfig wlan1 mode monitor
-sudo ifconfig wlan1 up
+sudo ip link set wlan1 down
+sudo iw dev wlan1 set monitor control
+sudo ip link set wlan1 up
+sudo ip link set wlan2 down
+sudo iw dev wlan2 set monitor control
+sudo ip link set wlan2 up
